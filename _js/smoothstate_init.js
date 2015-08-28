0142323aae5415b974b7156ae39dce5b3d16d5c3
etcoman <jasonetco@gmail.com>
2015-08-28 16:45:24 +0000

    Implemented smoothState, organized Sass partials

--- _js/smoothstate_init.js
+++ _js/smoothstate_init.js
@@ -1,29 +1,48 @@
-// SmoothState
-;(function ($) {
-    'use strict';
-    var $body    = $('html, body'),
-        content  = $('#main').smoothState({
-            prefetch: true,
-            pageCacheSize: 4,
-            onStart: {
-                duration: 600,
-                render: function (url, $container) {
-                    content.toggleAnimationClass('is-exiting');
-                    $body.animate({
-                        scrollTop: 0
-                    });
-                }
-            },
-
-            callback: function(url, $container, $content) {
-                // Call start function to use JS after smoothstate
-                start();
-                form_start();
-            },
-        }).data('smoothState');
-})(jQuery);
-
 $(document).ready(function() {
     start();
     form_start(); 
+});
+
+
+$(function(){
+  'use strict';
+  var $body    = $('html, body');
+  var options = {
+    prefetch: true,
+    cacheLength: 2,
+    
+    onStart: {
+      duration: 750, // Duration of our animation
+      render: function ($container) {
+        
+        // Add your CSS animation reversing class
+        $container.addClass('is-exiting');
+        
+        $body.animate({
+          scrollTop: 0
+        });
+        
+        // Restart your animation
+        smoothState.restartCSSAnimations();
+      }
+    },
+    
+    onReady: {
+      duration: 0,
+      render: function ($container, $newContent) {
+        // Remove your CSS animation reversing class
+        $container.removeClass('is-exiting');
+
+        // Inject the new content
+        $container.html($newContent);
+
+      }
+    },
+    
+    onAfter: function($container) {
+      start();
+      form_start();
+    }
+  },
+  smoothState = $('#main').smoothState(options).data('smoothState');
 });
