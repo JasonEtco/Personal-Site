3e3c68d06b6c741ec3e7bc32bbe12dc72ae1ff4c
jasonetco <jasonetco@gmail.com>
2016-03-01 13:21:23 -0500

    Slider pulls data from media-query width value

--- _js/scripts.js
+++ _js/scripts.js
@@ -13,9 +13,9 @@ var start = function() {
     var postNum    = post.length;
 	var next       = $('.work__next');
 	var prev       = $('.work__prev');
-    var postCount  = 4
 
-    post.css("width", "" + 100 / postCount + "%");
+    var postCount = container.width() / parseInt(post.css("width"));
+
     post.css({transform: 'translate3d(0,0,0)'});
 
     // Gets a pixel value of how much the elements should translate
@@ -24,15 +24,17 @@ var start = function() {
 
     // On NEXT button click
     next.click(function() {
+        // Recalculates postCount in case page has resized
+        var postCount = container.width() / parseInt(post.css("width"));
+        var step =  container.width() / postCount;
+
         // Get integer for current value of transform property
         // Takes [4] of the transform array which is the X value
         var current_pull = parseInt(post.css('transform').split(',')[4]);
-        var debug = (postNum * step) - (step * postCount)
 
         if(current_pull == (postNum * step - step * postCount) * -1) {
             console.log("I'm stopping here thanks");
         } else {
-            console.log(debug);
 
             // Variable for a new value
             // which is the sum of where it
@@ -46,7 +48,11 @@ var start = function() {
 
 
     // On PREVIOUS button click
-    prev.click(function() {    
+    prev.click(function() {
+        // Recalculates postCount and step in case page has resized
+        var postCount = container.width() / parseInt(post.css("width"));
+        var step =  container.width() / postCount;
+
         // Get integer for current value of transform property
         // Takes [4] of the transform array which is the X value
         var current_pull = parseInt(post.css('transform').split(',')[4]);
