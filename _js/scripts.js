162e7877c645d62e66e0f6ebb9a59ef75deedc73
jasonetco <jasonetco@gmail.com>
2016-03-04 01:12:36 -0500

    JS Slider now uses Vanilla JS

--- _js/scripts.js
+++ _js/scripts.js
@@ -11,65 +11,71 @@ var start = function() {
 ////////////////////////////////////////////////////////////////////////////////////////
 
     // SLIDER
-    var container  = $('.section--work');
-	var post       = $('.work__post');
-    var postNum    = post.length;
-	var next       = $('.work__next');
-	var prev       = $('.work__prev');
+    var workContainer  = document.querySelector('.section--work');
+	var post           = document.querySelectorAll('.work__post');
+	var next           = document.querySelector('.work__next');
+	var prev           = document.querySelector('.work__prev');
 
-    var postCount = container.width() / parseInt(post.css("width"));
+    if(workContainer) {
 
-    post.css({transform: 'translate3d(0,0,0)'});
+        var postCount = workContainer.offsetWidth / post[0].offsetWidth;
 
-    window.onresize = function() {
-        post.css({transform: 'translate3d(0,0,0)'});
+        [].forEach.call(post, function(e) {
+            e.style.transform = 'translate3d(0,0,0)';
+        });
+
+        window.onresize = function() {
+           [].forEach.call(post, function(e) {
+               e.style.transform = 'translate3d(0,0,0)';
+           });
+        }
+
+        // On NEXT button click
+        next.addEventListener("click", function() {
+            
+            // Recalculates postCount in case page has resized
+            var postCount = Math.round(workContainer.offsetWidth / post[0].offsetWidth);
+
+            // Get integer for current value of transform property
+            // Uses RegEx to get the right value
+            var current_pull = post[0].style.transform.match(/-?[\d\.]+/g)[1];
+
+            // Checks to see if the slider is at the end
+            if(current_pull != (post.length - postCount) * -100) {
+                // Variable for a new value
+                // which is the sum of where it
+                // is now plus the step variable
+                var new_pull = current_pull - 100;
+
+                // Applies the new variable and rounds
+                [].forEach.call(post, function(e) {
+                    e.style.transform = 'translate3d(' + new_pull + '%,0,0)';
+                });
+            }
+        });
+
+
+        // On PREVIOUS button click
+        prev.addEventListener("click", function() {
+            // Get integer for current value of transform property
+            // Uses RegEx to get the right value
+            var current_pull = post[0].style.transform.match(/-?[\d\.]+/g)[1];
+
+            // Checks if the transform is currently at 0
+            // to tell if the slider has moved yet
+            if(current_pull != 0) {
+                // Variable for a new value
+                // which is the sum of where it
+                // is now plus the step variable
+                var new_pull = parseInt(current_pull) + 100;
+
+                // Applies the new variable and rounds
+                [].forEach.call(post, function(e) {
+                    e.style.transform = 'translate3d(' + new_pull + '%,0,0)';
+                });
+            }
+        });
     }
-
-    // On NEXT button click
-    next.click(function() {
-        // Recalculates postCount in case page has resized
-        var postCount = Math.round(container.width() / parseInt(post.css("width")));
-
-        // Get integer for current value of transform property
-        // Uses RegEx to get the right value
-        var current_pull = post[0].style.transform.match(/-?[\d\.]+/g)[1];
-
-        // Checks to see if the slider is at the end
-        if(current_pull == -100 * (postNum - postCount)) {
-            // AT END
-        } else {
-            // Variable for a new value
-            // which is the sum of where it
-            // is now plus the step variable
-            var new_pull = current_pull - 100;
-
-            // Applies the new variable and rounds
-            post.css({transform: 'translate3d(' + new_pull + '%, 0, 0)'});
-        }
-    });
-
-
-    // On PREVIOUS button click
-    prev.click(function() {
-        // Get integer for current value of transform property
-        // Uses RegEx to get the right value
-        var current_pull = post[0].style.transform.match(/-?[\d\.]+/g)[1];
-
-        // Checks if the transform is currently at 0
-        // to tell if the slider has moved yet
-        if(current_pull == 0) {
-            // AT BEGINNING
-        } else {
-
-            // Variable for a new value
-            // which is the sum of where it
-            // is now plus the step variable
-            var new_pull = parseInt(current_pull) + 100;
-
-            // Applies the new variable and rounds
-            post.css({transform: 'translate3d(' + new_pull + '%, 0, 0)'});
-        }
-    });
     // END SLIDER
 
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -81,8 +87,6 @@ var start = function() {
             headerImage.classList.add('loaded');
         });
     }
-
-
 
     window.onscroll = function() {
         var pageHeight = window.innerHeight;
